e315534e32169d6af3705cc62aa8b32a7a55890c
Gabriela Vieira <gabivsvaz@gmail.com>
2024-07-10 00:18:39 -0300

    melhoria form

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,72 @@
+import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2 } from 'lucide-react';
+import { useState } from 'react';
+
 function App() {
+  const [isGuestInputOpen, setIsGuestInputOpen] = useState(false);
+
+  function openGuestInput() {
+    setIsGuestInputOpen(true);
+  }
+
+  function closeGuestInput() {
+    setIsGuestInputOpen(false);
+  }
+
+
   return (
-    <div className="h-screen flex items-center justify-center">
-      <div className="max-w-3xl max-w-full px-6 text-center space-y-10">
-        <p className="text-zinc-300 text-lg">Convide seus amigos e planeje a sua viagem!</p>
+    <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
+      <div className="max-w-3xl w-full px-6 text-center space-y-10">
 
-        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape">
-          <input type="text" placeholder="Para onde você vai?" className="bg-transparent" ></input>
-          <input type="text" placeholder="Quando?" className="bg-transparent"></input>
-
-          <button>Continuar</button>
+        <div className="flex flex-col items-center gap-3">
+          <img src="/logo.svg" alt="plann.er" />
+          <p className="text-zinc-300 text-lg">Convide seus amigos e planeje a sua viagem!</p>
         </div>
 
-          <p className="text-sn text-zinc-500">Ao planejar sua viagem pela planner você automaticamente concorda <br></br> com nossos <a className="text-zinc-300 underline" href="#">termos de uso</a> e <a className="text-zinc-300 underline" href="#">  políticas de privacidade.</a></p>
+      <div className='space-y-4'>
+
+        <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+          <div className="flex items-center gap-2 flex-1">
+            <MapPin className="size-5 text-zinc-400" />
+            <input disabled={isGuestInputOpen} type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+            <Calendar className="size-5 text-zinc-400" />
+            <input disabled={isGuestInputOpen} type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
+          </div>
+
+          <div className="w-px h-6 bg-zinc-800"></div>
+
+         {isGuestInputOpen ? (
+          <button onClick={closeGuestInput} className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
+             Alterar local/data <Settings2 className='size-5' />
+          </button>
+         ): (
+          <button onClick={openGuestInput} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'> Continuar</button>
+         )}
+        </div>
+
+        {isGuestInputOpen ? (
+          <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+          <div className="flex items-center gap-2 flex-1">
+            <UserRoundPlus className="size-5 text-zinc-400" />
+            <input type="text" placeholder="Quem estará na viagem?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+            
+          </div>
+
+          <div className="w-px h-6 bg-zinc-800"></div>
+
+          <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+            Confirmar viagem <ArrowRight className="size-5" />
+          </button>
+        </div>
+        ): null}
+      </div>
+
+        <p className="text-sn text-zinc-500">
+          Ao planejar sua viagem pela planner você automaticamente concorda <br />
+          com nossos <a className="text-zinc-300 underline" href="#">termos de uso</a> e <a className="text-zinc-300 underline" href="#">políticas de privacidade</a>.
+        </p>
       </div>
     </div>
- )
+  );
 }
 
-export default App
+export default App;
